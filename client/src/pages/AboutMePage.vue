085818948f14b819221937687264fc456ff5e882
vpriya22 <75938791+vpriya22@users.noreply.github.com>
2023-06-10 11:13:37 -0400

    bug fix

--- client/src/pages/AboutMePage.vue
+++ client/src/pages/AboutMePage.vue
@@ -14,8 +14,3 @@
     
     <!-- Add "scoped" attribute to limit CSS to this component only -->
     <style scoped></style>
-
-    To submit, go to blackboard and submit a link to your branch. 
-    This submission should just mock out some simple navigation -
-     for example a page saying "login goes here", a page saying "info about me",
-     a page saying "list all of my repos", and a page saying "repo details will go here". 
